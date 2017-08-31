78a197a8dc7cec4d8b9660be26021b890197e933
Sameet A. Ahmed <ahmed.sameet@gmail.com>
2017-08-31 19:48:44 -0400

    Update

--- app/src/main/java/com/sameet/popularmoviesv2/MainActivity.java
+++ app/src/main/java/com/sameet/popularmoviesv2/MainActivity.java
@@ -1,8 +1,5 @@
 package com.sameet.popularmoviesv2;
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -11,34 +8,12 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 import com.sameet.popularmoviesv2.fragments.mainactivity.FavoritesFragment;
 import com.sameet.popularmoviesv2.fragments.mainactivity.MoviesFragment;
-import com.sameet.popularmoviesv2.model.Movie;
-import com.sameet.popularmoviesv2.networkutils.NetworkRequest;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    private static final String API_KEY = "";
-    private static final String POPULAR_URL = "http://api.themoviedb.org/3/movie/popular/?api_key=" + API_KEY;
-    private static final String HIGHEST_RATED_URL = "http://api.themoviedb.org/3/movie/top_rated/?api_key=" + API_KEY;
-    private static final String POPULAR_KEY = "popularKey";
-    private static final String HIGHEST_RATED_KEY = "highestRatedKey";
-    private Movie mMovie;
-    private Movie popularMovie;
-    private Movie highestRatedMovie;
-    private ConnectivityManager mConnectivityManager;
-    private NetworkInfo mNetworkInfo;
-    private NetworkRequest mNetworkRequest;
     private FragmentManager mFragmentManager;
     private ViewPager mViewPager;
     private Toolbar mToolbar;
@@ -61,93 +36,8 @@ public class MainActivity extends AppCompatActivity {
 
         mTabLayout = (TabLayout) findViewById(R.id.tabs);
         mTabLayout.setupWithViewPager(mViewPager);
-
-        mNetworkRequest = NetworkRequest.getInstance(this); // Creating an instance of the network which will last lifetime of the activity
-
-
-        if(isNetworkAvailable()) {
-            loadData();
-            // Pass popularMovie and highestRatedMovie to the MovieFragment
-
-
-        }
-
-
-
     }
 
-    /**
-     * This method loads the Movie data in the main UI thread using Volley
-     **/
-    private void loadData() {
-        StringRequest popularRequest = new StringRequest(Request.Method.GET,
-                POPULAR_URL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            popularMovie = jsonParse(response); // Stores the popular movies in a popular movie object;
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-                });
-
-        StringRequest topRatedRequest = new StringRequest(Request.Method.GET,
-                HIGHEST_RATED_URL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            highestRatedMovie = jsonParse(response); // Stores the popular movies in a highest rated movie object
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-                });
-
-        mNetworkRequest.addToRequestQueue(popularRequest);
-        mNetworkRequest.addToRequestQueue(topRatedRequest);
-    }
-
-    private Movie jsonParse(String response) throws JSONException {
-        JSONObject jsonObject = new JSONObject(response);
-        JSONArray results = jsonObject.getJSONArray("results");
-        for (int i = 0; i < results.length(); i++) {
-            JSONObject objects = results.getJSONObject(i);
-            mMovie = new Movie(
-                    objects.getString("title"),
-                    objects.getString("poster_path"),
-                    objects.getString("overview"),
-                    objects.getDouble("vote_average"),
-                    objects.getString("release_date"),
-                    objects.getInt(null), // Null for trailerID as we'll get this in the DetailActivity
-                    objects.getString(null) // Null for the reviews as we'll get this in the DetailActivity
-            );
-        }
-        return mMovie;
-    }
-
-    /**
-     * This method checks for internet connectivty before requesting Movies from API
-     **/
-    private boolean isNetworkAvailable() {
-        mConnectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        mNetworkInfo = mConnectivityManager.getActiveNetworkInfo();
-        return mNetworkInfo != null && mNetworkInfo.isConnected();
-    }
 
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
--- app/src/main/java/com/sameet/popularmoviesv2/fragments/mainactivity/MoviesFragment.java
+++ app/src/main/java/com/sameet/popularmoviesv2/fragments/mainactivity/MoviesFragment.java
@@ -1,18 +1,50 @@
 package com.sameet.popularmoviesv2.fragments.mainactivity;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.sameet.popularmoviesv2.R;
+import com.sameet.popularmoviesv2.adapters.MovieAdapter;
+import com.sameet.popularmoviesv2.model.Movie;
+import com.sameet.popularmoviesv2.networkutils.NetworkRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MoviesFragment extends android.support.v4.app.Fragment {
+    private static final String API_KEY = "0ec5cc72e1380fbed537bfe349bcc4fb";
+    private static final String POPULAR_URL = "http://api.themoviedb.org/3/movie/popular/?api_key=" + API_KEY;
+    private static final String HIGHEST_RATED_URL = "http://api.themoviedb.org/3/movie/top_rated/?api_key=" + API_KEY;
+    private static final String POPULAR_KEY = "popularKey";
+    private static final String HIGHEST_RATED_KEY = "highestRatedKey";
+    private Movie mMovie;
+    private List<Movie> mMovies = new ArrayList<>();
+    private ConnectivityManager mConnectivityManager;
+    private NetworkInfo mNetworkInfo;
+    private NetworkRequest mNetworkRequest;
+    private MovieAdapter mMovieAdapter;
+    private RecyclerView mRecyclerView;
 
 
     public MoviesFragment() {
@@ -23,8 +55,96 @@ public class MoviesFragment extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mRecyclerView = (RecyclerView) container.findViewById(R.id.movies_recycler_view);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 3);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        mMovieAdapter = new MovieAdapter(mMovies, getContext());
+        mRecyclerView.setAdapter(mMovieAdapter);
+
+        if (isNetworkAvailable()) {
+            loadData(POPULAR_URL);
+        }
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movies, container, false);
+    }
+
+    /**
+     * This method loads the Movie data in the main UI thread using Volley
+     **/
+    private void loadData(String url) {
+        StringRequest popularRequest = new StringRequest(Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                           jsonParse(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        StringRequest topRatedRequest = new StringRequest(Request.Method.GET,
+               url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                       jsonParse(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        mNetworkRequest.addToRequestQueue(popularRequest);
+        mNetworkRequest.addToRequestQueue(topRatedRequest);
+    }
+
+    private void jsonParse(String response) throws JSONException {
+        mMovies.clear(); // Clears the list of movies
+        JSONObject jsonObject = new JSONObject(response);
+        JSONArray results = jsonObject.getJSONArray("results");
+        for (int i = 0; i < results.length(); i++) {
+            JSONObject objects = results.getJSONObject(i);
+            mMovie = new Movie(
+                    objects.getString("title"),
+                    objects.getString("poster_path"),
+                    objects.getString("overview"),
+                    objects.getDouble("vote_average"),
+                    objects.getString("release_date"),
+                    objects.getInt(null), // Null for trailerID as we'll get this in the DetailActivity
+                    objects.getString(null) // Null for the reviews as we'll get this in the DetailActivity
+            );
+            mMovies.add(mMovie);
+        }
+        mMovieAdapter.replaceData(mMovies); // Reloads movie data into adapter
+    }
+
+    /**
+     * This method checks for internet connectivty before requesting Movies from API
+     **/
+    private boolean isNetworkAvailable() {
+        mConnectivityManager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        mNetworkInfo = mConnectivityManager.getActiveNetworkInfo();
+        return mNetworkInfo != null && mNetworkInfo.isConnected();
     }
 
 }
